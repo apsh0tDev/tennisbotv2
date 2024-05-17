818281bfab8c3312cf0f1e92d612abb03efce21d
apsh0tDev <apsh0t@proton.me>
2024-05-17 19:47:41 -0400

    change

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ commands_message = f"""🤖 **Commands:**
 
                     `!live`: Displays a full list of today's live matches and current scores
                     `!schedule`: Displays a list of scheduled events
-                    `!sportbooks`: Displays a list of available sportbooks
+                    `!sportsbooks`: Displays a list of available sportbooks
                     """
 
 sportbooks_list = [
